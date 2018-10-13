472b0ba6b59c64c34d36c171af6d898d873fe0b1
ymtsk0131 <ymtsk0131@live.jp>
2018-10-13 18:59:54 +0900

    display all events

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,6 @@ const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
 
 export const readEvents = () => async dispatch => {
-    console.log(2)
     const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
-    console.log(response)
     dispatch({type: READ_EVENTS, response })
 }
--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,27 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-
+import _ from 'lodash'
 import { readEvents } from '../actions'
 
 class EventsIndex extends Component {
   componentDidMount() {
-    console.log(1)
     this.props.readEvents()
   }
 
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key = { event.id }>
+        <td>{ event.id }</td>
+        <td>{ event.title }</td>
+        <td>{ event.body }</td>
+      </tr>
+    ))
+  }
+
   render() {
-    const props = this.props
     return (
-      <React.Fragment>
-        <div>value: { props.value }</div>
-        <button onClick={props.increment}>+1</button>
-        <button onClick={props.decrement}>-1</button>
-      </React.Fragment>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Boby</th>
+          </tr>
+        </thead>
+        <tbody>
+          { this.renderEvents() }
+        </tbody>
+      </table>
     )
   }
 }
 
-const mapStateToProps = state => ({})
+const mapStateToProps = state => ({ events: state.events })
 const mapDispatchToProps = ({ readEvents })
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
