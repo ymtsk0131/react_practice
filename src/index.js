c58bd18ea898fc66f81e062d8dae47d5fe5330ea
ymtsk0131 <ymtsk0131@live.jp>
2018-12-31 17:18:12 +0900

    create delete event function

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show';
 import registerServiceWorker from './registerServiceWorker';
 
 const enhancer = process.env.NODE_ENV === 'development' ?
@@ -20,8 +21,10 @@ ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <Switch>
-                <Route exact path="/events/new" component={EventsNew} />
+                <Route path="/events/new" component={EventsNew} />
+                <Route path="/events/:id" component={EventsShow} />
                 <Route exact path="/" component={EventsIndex} />
+                <Route exact path="/events" component={EventsIndex} />
             </Switch>
         </BrowserRouter>
     </Provider>,
